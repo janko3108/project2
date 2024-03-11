cfb1cc11d3e9b2c7f6c3137fab94db167690412c
jleskovac <jl8592@rit.edu>
2024-03-11 21:09:14 +0100

    Testing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// App.js
+
 import React from "react";
 import './App.css';
 import About from './components/About';
@@ -5,17 +7,19 @@ import Degrees from "./components/Degrees";
 import Employment from "./components/Employment";
 import Faculty from "./components/Faculty";
 import Contact from "./components/Contact";
+import ResponsiveNavbar from './components/ResponsiveNavbar'; // Import the new component
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <div className='App'>
-                <section id='About'><About /></section>
-                <section id='Degrees'><Degrees /></section>
-                <section id='Employment'><Employment /></section>
-                <section id='Faculty'><Faculty /></section>
-                <section id='Contact'><Contact /></section>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='App'>
+        <ResponsiveNavbar /> {/* Use the responsive navbar component */}
+        <section id='About'><About /></section>
+        <section id='Degrees'><Degrees /></section>
+        <section id='Employment'><Employment /></section>
+        <section id='Faculty'><Faculty /></section>
+        <section id='Contact'><Contact /></section>
+      </div>
+    );
+  }
 }
